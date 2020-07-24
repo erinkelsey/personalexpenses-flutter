8940cbb1b34a9a683693dd2928613b2cf26af39d
erinkelsey <erinszilagyi@gmail.com>
2020-07-24 13:53:32 -0500

    add new transactions and scrollable view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-import './transaction.dart';
+import './widgets/user_transactions.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,26 +18,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1', 
-      title: 'New Shoes', 
-      amount: 69.99, 
-      date: DateTime.now()
-    ),
-    Transaction(
-      id: 't2', 
-      title: 'Weekly Groceries', 
-      amount: 120.59, 
-      date: DateTime.now()
-    ),
-  ];
-
-  // String titleInput;
-  // String amountInput;
-
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -46,98 +25,23 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        // crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            constraints: BoxConstraints(minWidth: 600, maxWidth: 1500),
-            child: Card(
-              child: Text('CHARTTT'),
-              color: Colors.blue,
-              elevation: 5,
-            )
-          ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: titleController,
-                    // onChanged: (value) {
-                    //   titleInput = value;
-                    // },
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    // onChanged: (value) => amountInput = value,
-                    controller: amountController,
-                  ),
-                  FlatButton(
-                    child: Text('Add Transaction'), 
-                    onPressed: () {
-                      print(titleController.text);
-                      print(amountController.text);
-                    },
-                    textColor: Colors.purple,
-                  ),
-                ]
-              ),
+      body: SingleChildScrollView(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              constraints: BoxConstraints(minWidth: 600, maxWidth: 1500),
+              child: Card(
+                child: Text('CHARTTT'),
+                color: Colors.blue,
+                elevation: 5,
+              )
             ),
-          ),
-        Column(
-          children: transactions.map((tx) {
-          return Card(
-              child: Row(children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10, 
-                    horizontal: 15
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '\$${tx.amount}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tx.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    Text(
-                      DateFormat.yMMMMd().format(tx.date),
-                      style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ]
-                ),
-              ],
-              ),
-          );
-        }).toList(),),
-      ],)
+            UserTransactions(),
+          ],
+        ),
+      ),
     );
   }
 }
